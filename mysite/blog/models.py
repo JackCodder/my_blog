34bf688c9972bcf0877aefc58599b0fe7afaac2a
JackCodder <not.a.jack.shenzhen@gmail.com>
2024-04-11 11:30:57 +0500

    Добавление поля статуса и активация приложения

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+
+    class Status(models.TextChoices): #Добавление поля статуса
+        DRAFT = 'DF', 'Draft'
+        PUDLISHED = 'PB', 'Published'
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
